ac2a60104830ae665fcb13c34041f0bf66ac046f
DanielVTS <54309881+DanielVTS@users.noreply.github.com>
2020-12-01 09:51:12 +0800

    添加接口逻辑中

--- Locker_Services/src/main/java/cn/lingnan/controller/LockerBasicInformationController.java
+++ Locker_Services/src/main/java/cn/lingnan/controller/LockerBasicInformationController.java
@@ -9,19 +9,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
-@RequestMapping("lockerbasicinformation")
+@RequestMapping("lockerBasicInformation")
 @Controller
 public class LockerBasicInformationController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     @Resource
     private LockerBasicInformationService lockerBasicInformationService;
 
+    @PostMapping("addLocker")
     public CommonResult<String> AddLocker(@RequestBody @Validated LockerBasicInformation record){
+        logger.debug("添加Locker ==>"+record.toString());
         int result=lockerBasicInformationService.insert(record);
         if (result!=1) {
             throw new APIException(500,"LBI记录插入异常！");
@@ -29,20 +32,34 @@ public class LockerBasicInformationController {
         return CommonResult.success("LBI记录插入成功！");
     }
 
+    @PostMapping("removeLocker")
     public CommonResult<String> RemoveLocker(@RequestBody @Validated LockerBasicInformation record){
+        logger.debug("删除Locker ==>"+record.toString());
         int result=lockerBasicInformationService.deleteByPrimaryKey(record.getLockerId());
         if (result!=1) {
             throw new APIException(500,"LBI记录删除异常！");
         }
         return CommonResult.success("LBI记录删除成功！");
     }
-
+    @PostMapping("editLocker")
     public CommonResult<String> EditLocker(@RequestBody @Validated LockerBasicInformation record){
+        logger.debug("修改Locker ==>"+record.toString());
         int result=lockerBasicInformationService.updateByPrimaryKey(record);
         if (result!=1) {
-            throw new APIException(500,"LBI记录删除异常！");
+            throw new APIException(500,"LBI记录修改异常！");
         }
         return CommonResult.success("LBI记录删除成功！");
+    }
+
+    @PostMapping("findLockerById")
+    public CommonResult<?> FindLocker(int id){
+        logger.debug("ID查找Locker ==>ID: "+id);
+        LockerBasicInformation record=lockerBasicInformationService.selectByPrimaryKey(id);
+        if (record!=null){
+            return CommonResult.success(record);
+        }else {
+            return CommonResult.success("无此快递柜记录！");
+        }
     }
 
 
