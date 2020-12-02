252d3c669df2d113365db875f9cb2281203d7576
DanielVTS <54309881+DanielVTS@users.noreply.github.com>
2020-12-02 10:41:11 +0800

    测试镜像构建

--- Locker_Services/src/main/java/cn/lingnan/LockerServicesApp.java
+++ Locker_Services/src/main/java/cn/lingnan/LockerServicesApp.java
@@ -2,10 +2,16 @@ package cn.lingnan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class LockerServicesApp {
     public static void main(String[] args) {
         SpringApplication.run(LockerServicesApp.class,args);
+    }
+
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
