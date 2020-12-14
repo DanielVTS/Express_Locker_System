adbbee918de9b8af7b50c6f210119606df67446d
DanielVTS <54309881+DanielVTS@users.noreply.github.com>
2020-12-14 21:33:55 +0800

    Adding LockerBox Interface

---
+++ Locker_Services/src/main/java/cn/lingnan/controller/LockerBoxInformationController.java
@@ -0,0 +1,54 @@
+package cn.lingnan.controller;
+
+import cn.lingnan.dto.LockerBoxInformation;
+import cn.lingnan.exception.APIException;
+import cn.lingnan.service.LockerBoxInformationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@RequestMapping("lockerBoxInformation")
+@Controller
+public class LockerBoxInformationController {
+    private final Logger logger = LoggerFactory.getLogger(LockerBoxInformationService.class);
+    @Resource
+    private LockerBoxInformationService lockerBoxInformationService;
+
+    @PostMapping("addLockerBox")
+    public String AddLockerBox(@RequestBody @Validated LockerBoxInformation record) {
+        logger.info("添加LockerBox ==>" + record.toString());
+        int result = lockerBoxInformationService.insert(record);
+        if (result != 1) {
+            throw new APIException(500, "Box记录插入异常！");
+        }
+        return "Box记录插入成功！";
+    }
+
+    @PostMapping("removeLockerBox")
+    public String RemoveLockerBox(@RequestBody @Validated LockerBoxInformation record) {
+        logger.info("删除Locker ==>" + record.toString());
+        int result = lockerBoxInformationService.deleteByPrimaryKey(record.getLockerId());
+        if (result != 1) {
+            throw new APIException(500, "Box记录删除异常！");
+        }
+        return "Box记录删除成功！";
+    }
+
+    @PostMapping("editLockerBox")
+    public String EditLockerBox(@RequestBody @Validated LockerBoxInformation record) {
+        logger.info("修改Locker ==>" + record.toString());
+        int result = lockerBoxInformationService.updateByPrimaryKey(record);
+        if (result != 1) {
+            throw new APIException(500, "Box记录修改异常！");
+        }
+        return "Box记录修改成功！";
+    }
+
+
+}
