b0683bba441fee1aa1a9e08f6b3792238b80b8af
DanielVTS <caitanghan@gmail.com>
2021-04-23 16:55:18 +0800

    Adding k8s probe

--- Locker_Services/src/main/java/cn/lingnan/LockerServicesApp.java
+++ Locker_Services/src/main/java/cn/lingnan/LockerServicesApp.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class LockerServicesApp {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(LockerServicesApp.class,args);
+        SpringApplication.run(LockerServicesApp.class, args);
     }
 
     protected SpringApplicationBuilder configure(
--- Package_Services/src/main/java/cn/lingnan/PackageServicesApp.java
+++ Package_Services/src/main/java/cn/lingnan/PackageServicesApp.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PackageServicesApp {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PackageServicesApp.class, args);
     }
